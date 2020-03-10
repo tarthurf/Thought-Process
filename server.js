f33c3c89f343eab634a35fdc1eb1ba44f6e052af
Swedish <tarthurf@yahoo.com>
2020-03-10 14:31:29 -0700

    added post function to api/notes that appends a new note to an array and writes the array to db.json

--- server.js
+++ server.js
@@ -11,35 +11,64 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// set route paths
+// set paths
+const uniqueId = Date.now();
+const noteArray = [];
+
 // route for index
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'Develop/public' });
 });
 
 // route for notes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile('notes.html', { root: 'Develop/public' });
 });
 
 // route for api/notes
 app.get('/api/notes', (req, res) => {
-    return res.json('Develop\db\db.json'); //TODO: Look up res.json from different directory
+
+    let jsonNotes = fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'), 'utf8')
+    return res.json(jsonNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    // attach unique id to new notes (date object?)
+    // const newNote = req.body;
+
+    noteObject = {
+        title : req.body.title,
+        text: req.body.text,
+        ID: uniqueId
+    }
+    noteObject ? noteArray.unshift(noteObject) : console.error();
+    
+    
+    fs.writeFileSync(path.join(__dirname, '/Develop/db/db.json'), JSON.stringify(noteArray));
+
+    console.log(noteObject);
+
+    res.json(noteObject);
+    
+    // append new note to db.json
+    
+    // return new note
+    
+    // create note delete function
+
+
 });
 
 // Create post request for new note
 app.post('/api/notes:id', (req, res) => {
+
     
 })
-// attach unique id to new notes (date object?)
 
-// append new note to db.json
-
-// return new note
-
-// create note delete function
+console.log(fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'), 'utf8'))
 
 // Turn on server
 app.listen(PORT, () => {
     console.log('App lsitening to port ' + PORT);
+    console.log('The time is: ' + uniqueId);
 });
